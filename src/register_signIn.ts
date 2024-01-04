eebf322dc9a306df55e88c6c0baacf38f5addd0b
k1llah <surenbound@yandex.ru>
2024-01-05 02:12:50 +0300

    ready smooth inputs

--- src/register_signIn.ts
+++ src/register_signIn.ts
@@ -1,1 +1,16 @@
 import "./styleSignIn.css";
+let inputSignUp = document.querySelector('.input_sign_up') as HTMLDivElement
+let signIn = document.querySelector('.sign_in_block') as HTMLDivElement
+let column = document.querySelector('.column') as HTMLDivElement
+column.addEventListener('click', (event) =>{
+	let target = event.target as HTMLElement
+	if (target.classList.contains('register') && !signIn.classList.contains('active')) {
+    inputSignUp.classList.toggle('active')
+	}
+})
+column.addEventListener('click', (event) =>{
+	let target = event.target as HTMLElement
+	if (target.classList.contains('sign_in_2') && !inputSignUp.classList.contains('active')) {
+   signIn.classList.toggle('active')
+	}
+})
